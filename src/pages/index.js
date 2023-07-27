3a535824242fc7ad9833ad0a208af53177f3cbdc
VISHAL GUPTA <vishalgupta7684@gmail.com>
2023-07-27 13:20:45 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from"./HomePage";
+import Courses from "./CoursesPage";
+import SingleCourse from "./SingleCoursePage";
+import Cart from "./CartPage";
+import Login from "./Login";
+
+export {Home, Courses, SingleCourse, Cart, Login};
